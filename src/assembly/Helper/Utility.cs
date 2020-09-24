1ec1849803ab2b58597d918a988ce5e17fba09bf
The-New-Guy <The-New-Guy@users.noreply.github.com>
2020-09-24 11:23:51 -0400

    WIP: Stashing unfinished thought on a new branch
    Looking for a way to reduce the amount of duplicated code checking for 'Not Found' errors and retrying as a file. This code started by trying to solve that issue with Actions. Not sure if that makes sense still but don't want to throw this code away so I'll put it on a separate branch for a while.

--- src/assembly/Helper/Utility.cs
+++ src/assembly/Helper/Utility.cs
@@ -55,6 +55,85 @@ namespace PoshBox.Helper
                 action(file);
         }
 
+        /// <summary>
+        /// Apply the specified action to the requested Box item.
+        /// </summary>
+        /// <param name="client">An authenticated Box client.</param>
+        /// <param name="itemId">The ID of the BoxItem to retrieve.</param>
+        /// <param name="action">An action to apply to the Box files and folders. This can be any method that accepts a BoxItem as a parameter.</param>
+        /// <param name="errorAPIAction">(Optional) An action that takes an Exception object to handle any errors returned by the Box API call. This can be any method that accepts an Exception as a parameter.</param>
+        /// <param name="verboseAPIAction">(Optional) An action that takes a string and displays it as verbose information for each Box API call made. This can be any method that accepts a string as a parameter.</param>
+        /// <param name="properties">(Optional) A string array of Box file/folder properties to fetch with every folder item.</param>
+        /// <param name="itemType">(Optional) The type of item to be retrieved. This is not needed but if provided can save an extra API call to Box and reduce rate limit counts.</param>
+        /// <exception cref="System.Exception">
+        /// Throws when errors occur with the Box API. Inner exception will contain the original exception. Typically a BoxException object.
+        /// </exception>
+        public static void ApplyBoxItem(
+            BoxClient client,
+            string itemId,
+            Action<BoxItem> action,
+            Action<Exception> errorAPIAction = null,
+            Action<String> verboseAPIAction = null,
+            string[] properties = null,
+            string itemType = null
+        ) {
+            BoxItem item = null;
+
+            if (itemType == null || String.Equals(itemType, "Folder", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // Folder action.
+                try
+                {
+                    ApplyVerboseAPIAction(verboseAPIAction, "Retrieving folder: " + itemId);
+                    item = client.FoldersManager.GetInformationAsync(itemId, fields: properties).Result;
+                }
+                catch (AggregateException ae)
+                {
+                    foreach (var e in ae.Flatten().InnerExceptions) {
+                        // If no ItemType was given and we got a 404, try checking to see if its a file ID.
+                        if (e is BoxException && ((BoxException)e).StatusCode.ToString() == "NotFound")
+                        {
+                            if (itemType == null)
+                            {
+                                ApplyVerboseAPIAction(verboseAPIAction, "Item not found as a folder. Retrying as a file.");
+                                ApplyVerboseAPIAction(verboseAPIAction, "Retrieving file: " + itemId);
+                                ApplyBoxItem(client, itemId, action, errorAPIAction, verboseAPIAction, properties, itemType = "File");
+                            }
+                            else
+                                ApplyErrorAPIAction(errorAPIAction, "Folder not found: " + itemId, e);
+
+                        }
+                        else
+                            ApplyErrorAPIAction(errorAPIAction, "Error retrieving item: " + itemId, e);
+
+                    }
+                }
+            }
+            else
+            {
+                // File action.
+                try
+                {
+                    ApplyVerboseAPIAction(verboseAPIAction, "Retrieving file: " + itemId);
+                    item = client.FilesManager.GetInformationAsync(itemId, fields: properties).Result;
+                }
+                catch (AggregateException ae)
+                {
+                    foreach (var e in ae.Flatten().InnerExceptions) {
+                        // Just want to provide a more helpful error message for the common 404 Not Found error.
+                        if (e is BoxException && ((BoxException)e).StatusCode.ToString() == "NotFound")
+                            ApplyErrorAPIAction(errorAPIAction, "File not found: " + itemId , e);
+                        else
+                            ApplyErrorAPIAction(errorAPIAction, "Error retrieving file: " + itemId , e);
+                    }
+                }
+            }
+
+            if (item != null)
+                action(item);
+
+        }
+
         // Applies custom error action.
         private static void ApplyErrorAPIAction(Action<Exception> errorAction, string msg, Exception origException)
         {
